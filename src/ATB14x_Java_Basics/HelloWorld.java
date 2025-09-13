46ef3db77d309400431862722f15621c205f55bf
Swapnali <khairnarswapnalij@gmail.com>
2025-09-13 10:03:38 +0530

    13 sept part1

---
+++ src/ATB14x_Java_Basics/HelloWorld.java
@@ -0,0 +1,8 @@
+package ATB14x_Java_Basics;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println ("Hello World");
+        System.out.println("Hi Swapnali Khairnar");
+    }
+}
---
+++ src/ATB14x_Java_Basics/comments.java
@@ -0,0 +1,15 @@
+package ATB14x_Java_Basics;
+
+public class comments {
+//    Hi World
+    /*
+    *
+    *
+    * */
+
+
+
+    public static void main(String[] args) {
+        System.out.println(4+4);
+    }
+}
